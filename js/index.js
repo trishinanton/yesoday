b4183e1880b49e6ec274c0a72184dfe2795d2dfd
trishinanton <74793031+trishinanton@users.noreply.github.com>
2022-03-19 05:54:46 -0700

    Update index.js
    edit url

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ function onHoverLogo () {
     let logoContainer = document.querySelector('.menu__logo')
 
     logoContainer.addEventListener('mouseover', function() {
-        this.children[0].setAttribute('src', '/img/main/logo-active.png'); 
+        this.children[0].setAttribute('src', '{{ site.baseurl }}/img/main/logo-active.png'); 
         this.children[0].style.height = '61px'
         this.classList.add('menu__logo_active')
     })
 
     logoContainer.addEventListener('mouseout', function() {
-        this.children[0].setAttribute('src', '/img/main/logo.png');
+        this.children[0].setAttribute('src', '{{ site.baseurl }}/img/main/logo.png');
         this.classList.remove('menu__logo_active');
         this.children[0].style.height = ''
     })
